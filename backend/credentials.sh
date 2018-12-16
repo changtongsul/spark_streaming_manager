ad6538a84476ca68efc47238053acf13091d7d40
Do Il Yoon <ttotto3080@gmail.com>
2018-12-16 23:39:41 +0900

    delete credentials

--- backend/credentials.sh
+++
@@ -1,8 +0,0 @@
-export PG_DATABASE="ss_manager"
-export PG_ID="postgres"
-export PG_PASSWORD=""
-export PG_HOST="localhost"
-export JWT_KEY="ss-manager-key"
-export JWT_EXP_DATE="30d"
-export RM_URL="ec2-13-124-170-169.ap-northeast-2.compute.amazonaws.com"
-export RM_PORT="8088"
